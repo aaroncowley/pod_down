3569a9b8f8f7c821d4d822f925ee53fdd80192b2
acow <acow777@gmail.com>
2020-03-22 12:57:47 -0600

    ready for http downloader

--- main.cc
+++ main.cc
@@ -10,15 +10,32 @@ using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
+
 using namespace rapidxml;
 
-int main(void)
+string XMLDownload(string url)
+{
+
+}
+
+void MP3Download(string url)
+{
+    HTTPDownloader downloader;
+    string content
+}
+
+int main(int argc, char **argv)
 {
     cout << "Parsing PODCAST feed" << endl;
     xml_document<> doc;
     xml_node<> * root_node;
 
-    file<> rssFile("feed.xml");
+    if (argc != 2) {
+        cout << "No URL supplied" << endl;
+        return 1;
+    }
+
+    file<> rssFile(XMLDownload(url));
 
     doc.parse<0>(rssFile.data());
     root_node = doc.first_node("rss");
@@ -31,12 +48,16 @@ int main(void)
     root_node = root_node->first_node("channel");
     cout << "PODCAST Title: " << root_node->first_node("title")->value() << endl;
 
+    /* Map for storing podcast title to episode url */
+    map<int, string> podcast;
+
     for (xml_node<> * episode_node = root_node->first_node("item"); episode_node; episode_node = episode_node->next_sibling("item")){
-	    printf("Episode %s in %s. ", 
-	    	episode_node->first_node("title")->value(),
-	    	episode_node->first_node("enclosure")->first_attribute("url")->value());
-            // Interate over the beers 
-	    cout << endl;
+	    	podcast.insert(episode_node->first_node(episode_node->first_node("title")->value(), 
+	    	            episode_node->first_node("enclosure")->first_attribute("url")->value());
 	}
+
+	
+
+	return 0;
 }
     
